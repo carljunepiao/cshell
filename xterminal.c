2ad8824dc0b8a347ffb6ca5c4d12c57b10a81dd6
Aljomai <cgpiao1@up.edu.ph>
2017-11-13 04:11:20 +0800

    Start xterminal

---
+++ xterminal.c
@@ -0,0 +1,18 @@
+/*
+    Project name: xterminal
+    Description: An implementation of a kernel interface with basic commands.
+    Author: Charlito G. Piao Jr.
+
+    Compiler used: GPP Compiler
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// main entry point
+int main(int argc, char **argv)
+{
+    printf("Generate PI in the terminal\n");
+    return 0;
+}
