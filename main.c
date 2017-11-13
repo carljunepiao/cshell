e9a58089817fbb123d733a9c2e71028554820c9d
Aljomai <cgpiao1@up.edu.ph>
2017-11-13 21:50:14 +0800

    Ask input loop

--- main.c
+++ main.c
@@ -5,14 +5,27 @@
     Compiler: GPP Compiler
 */
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+
 // main entry point
 int main(int argc, char **argv)
 {
-    printf("Generate PI in the terminal based on the \n");
+    printf("\n\n\t\t\t~ Welcome to piterminal");
+
+    printf("\n\n\t\tGenerate PI in the terminal based on the \n");
+
+    int c;
+
+    c = getchar();
+
+    while (c != EOF) {
+        /* code */
+        putchar(c);
+        c = getchar();
+    }
+
     return 0;
 }
