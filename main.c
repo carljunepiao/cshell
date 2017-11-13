f9678a7b6b890c5c1263d9c53bdff435ead654e6
Aljomai <cgpiao1@up.edu.ph>
2017-11-13 19:21:47 +0800

    tes push

--- main.c
+++ main.c
@@ -9,40 +9,9 @@
 #include <stdio.h>
 #include <string.h>
 
-//read a line in the piminal
-#define LSH_RL_BUFSIZE 1024
-char *_read(void){
-
-}
-
-char *_split(){
-
-}
-
-char *_execute(){
-
-}
-
-int _input(){
-    char *line;
-    char **args;
-    bool status = true;
-
-    while (status) {
-        printf("~ ");
-        line = _read();
-        args = _split(line);
-        status = _execute(args);
-    }
-
-    return 0;
-}
-
 // main entry point
 int main(int argc, char **argv)
 {
-
     printf("Generate PI in the terminal based on the \n");
-    _input();
     return 0;
 }
